b08ca3f2210f28a0cb877e8b622e511dd9f27f71
sara alhasan <33129878+sarasaj@users.noreply.github.com>
2024-09-29 09:16:29 +0300

    Updates

---
+++ precache-manifest.6a0abda9be2949e02042002f0f6a670d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a21a64b98c4f8a778a85f45e041c45c",
+    "url": "/symptomchecker/index.html"
+  },
+  {
+    "revision": "77e181d51ad1a8f0acee",
+    "url": "/symptomchecker/static/css/2.687019c3.chunk.css"
+  },
+  {
+    "revision": "bda34af80d703ffaa6f7",
+    "url": "/symptomchecker/static/css/main.2013c5eb.chunk.css"
+  },
+  {
+    "revision": "77e181d51ad1a8f0acee",
+    "url": "/symptomchecker/static/js/2.312bc8c1.chunk.js"
+  },
+  {
+    "revision": "13e061b2fd50c1d7f7b7e0bb533040d6",
+    "url": "/symptomchecker/static/js/2.312bc8c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda34af80d703ffaa6f7",
+    "url": "/symptomchecker/static/js/main.ac6d69e6.chunk.js"
+  },
+  {
+    "revision": "1c72b753f81fe8f1be24",
+    "url": "/symptomchecker/static/js/runtime-main.b499f4de.js"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/symptomchecker/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/symptomchecker/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/symptomchecker/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/symptomchecker/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/symptomchecker/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/symptomchecker/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/symptomchecker/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/symptomchecker/static/media/fa-v4compatibility.7c428138.woff2"
+  },
+  {
+    "revision": "9de0a5451a50f43fa6a533f6f931cc35",
+    "url": "/symptomchecker/static/media/lan.9de0a545.png"
+  },
+  {
+    "revision": "8bf2a7b129aec43f9f73e2a722aa1a08",
+    "url": "/symptomchecker/static/media/mem.8bf2a7b1.jpg"
+  },
+  {
+    "revision": "a9b6ea15ce6ea660e82c2e2f7f78041c",
+    "url": "/symptomchecker/static/media/newlogo.a9b6ea15.svg"
+  },
+  {
+    "revision": "856d3a286c470cd1e6ed7315c7091c23",
+    "url": "/symptomchecker/static/media/per.856d3a28.jpg"
+  }
+]);
